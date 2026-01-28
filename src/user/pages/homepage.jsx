e98140386c9c5bbb20c80f4fdced9599b38fc3ae
Allen-pogi <systemadmin@primegroup.com.ph>
2026-01-28 11:43:23 +0800

    updatew

--- src/user/pages/homepage.jsx
+++ src/user/pages/homepage.jsx
@@ -183,7 +183,7 @@ const LandingPage = () => {
       <footer className="border-t border-primary/20 dark:border-primary/30">
         <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
           <div className="flex justify-center space-x-6 md:order-2">
-            <a
+            {/* <a
               href="#"
               className="text-gray-500 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-300"
             >
@@ -206,7 +206,7 @@ const LandingPage = () => {
               className="text-gray-500 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-300"
             >
               Terms of Service
-            </a>
+            </a> */}
           </div>
           <div className="mt-8 md:order-1 md:mt-0">
             <p className="text-center text-xs leading-5 text-gray-500 dark:text-gray-400">
--- src/website pages/HomePage.jsx
+++ src/website pages/HomePage.jsx
@@ -272,7 +272,7 @@ const WebsiteMain = () => {
       <footer className="border-t mt-8 border-primary/20 dark:border-primary/30">
         <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
           <div className="flex justify-center space-x-6 md:order-2">
-            <a
+            {/* <a
               href="#"
               className="text-gray-500 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-300"
             >
@@ -295,7 +295,7 @@ const WebsiteMain = () => {
               className="text-gray-500 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-300"
             >
               Terms of Service
-            </a>
+            </a> */}
           </div>
           <div className="mt-8 md:order-1 md:mt-0">
             <p className="text-center text-xs leading-5 text-gray-500 dark:text-gray-400">
